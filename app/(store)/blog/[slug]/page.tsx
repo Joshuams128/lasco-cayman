0a9e3af55b21f9a53f973b7ee9fa9a3a8d4763ad
Joshua <145717861+Joshuams128@users.noreply.github.com>
2026-04-05 02:20:32 +0000

    fix

--- app/(store)/blog/[slug]/page.tsx
+++ app/(store)/blog/[slug]/page.tsx
@@ -1,18 +1,20 @@
 // TODO: Fetch blog post by slug from Sanity
 // import { client } from "@/sanity/client";
 
-interface BlogPostPageProps {
-  params: { slug: string };
-}
+export default async function BlogPostPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
 
-export default function BlogPostPage({ params }: BlogPostPageProps) {
   // TODO: Fetch post data
-  // const post = await client.fetch(`*[_type == "post" && slug.current == $slug][0]`, { slug: params.slug });
+  // const post = await client.fetch(`*[_type == "post" && slug.current == $slug][0]`, { slug });
 
   return (
     <div className="mx-auto max-w-3xl px-4 py-8">
       <p className="text-gray-500">
-        TODO: Fetch and render blog post for slug: {params.slug}
+        TODO: Fetch and render blog post for slug: {slug}
       </p>
 
       {/* TODO: Post title, image, date, and block content */}
